16fdba65ec2326d7a706b0358f985bd9c0a8fff2
hovo-dev <hovhannes.baghdasaryan.03@gmail.com>
2024-02-10 01:05:01 +0400

    add phone number

--- src/api/services/codeService.ts
+++ src/api/services/codeService.ts
@@ -2,29 +2,37 @@ import {Response} from "express"
 import {transporter} from "../../config/mail.ts";
 import {E_AUTH_TYPE} from "../../config/consts.ts";
 import {otpGenerate} from "../../util";
-import {compareWithEncrypted, hash} from "../../util/hash.ts";
-import {AUTH_TYPE} from "../../config/env.ts";
+import {hash} from "../../util/hash.ts";
+import client from "../../config/twilio.ts";
 
 class CodeService {
-    async sendOtp(login: string, authType: E_AUTH_TYPE, res: Response): Promise<string | undefined> {
+    async sendOtp(username: string, authType: E_AUTH_TYPE, res: Response): Promise<string | undefined> {
         try {
             const newOtp = otpGenerate()
 
             switch (authType) {
                 case E_AUTH_TYPE.EMAIL:
                     await transporter.sendMail({
-                        to: login,
+                        to: username,
                         subject: 'WELCOME: AUTHORIZE WITH E-MAIL',
                         text: `Code: ${newOtp}`
                     })
+                    break
 
-                    console.info("Mail sent successfully")
+                case E_AUTH_TYPE.PHONE:
+                    await client.messages.create({
+                        body: `Code: ${newOtp}`,
+                        to: `+${username}`,
+                        from: process.env.TWILIO_PHONE_NUMBER
+                    })
             }
+
+            console.info("OTP sent successfully")
 
             return hash(newOtp)
         } catch (error) {
             console.error(error)
-            res.status(500).json({error: `sendMailOtpCodeService: mail not sent`})
+            res.status(500).json({error: `sendOtpCodeService: OTP not sent`})
         }
     }
 }
